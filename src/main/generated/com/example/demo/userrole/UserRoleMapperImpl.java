ae1f9227abdbbc584c27775555b76f57f605e81d
annvool <annvool@gmail.com>
2022-04-27 15:57:13 +0300

    Ann commit "toEntity"

--- src/main/generated/com/example/demo/userrole/UserRoleMapperImpl.java
+++ src/main/generated/com/example/demo/userrole/UserRoleMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-04-27T15:53:06+0300",
+    date = "2022-04-27T15:56:31+0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
 )
 @Component
