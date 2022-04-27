d5cc93521afb05a831b1893ded5d295966a3b760
annvool <annvool@gmail.com>
2022-04-27 15:09:24 +0300

    Ann commit Education experience classes

--- src/main/generated/com/example/demo/user/UserMapperImpl.java
+++ src/main/generated/com/example/demo/user/UserMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-04-27T13:57:19+0300",
+    date = "2022-04-27T15:07:55+0300",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
 )
 @Component
