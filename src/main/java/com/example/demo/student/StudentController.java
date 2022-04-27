fa5b2f88a8c6f2341516872c2c81f228d5189387
annvool <annvool@gmail.com>
2022-04-27 14:54:23 +0300

    Ann commit Student classes

---
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,14 @@
+package com.example.demo.student;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    @Resource
+    private StudentService studentService;
+}
---
+++ src/main/java/com/example/demo/student/StudentDto.java
@@ -0,0 +1,24 @@
+package com.example.demo.student;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data
+public class StudentDto implements Serializable {
+    private final Integer id;
+    private final Integer userId;
+    private final String userUserName;
+    private final String userPassword;
+    private final Integer studentProfileId;
+    private final String studentProfileAvailableFrom;
+    private final String studentProfileLocation;
+    private final LocalDate studentProfileDateOfBirth;
+    private final String studentProfileCompetence;
+    private final String studentProfileStrength;
+    private final String studentProfileAboutMe;
+    private final String studentProfileLinkedin;
+    private final String studentProfileGithubLink;
+    private final Boolean isAvailable;
+}
