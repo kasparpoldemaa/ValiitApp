f229721ee29b152ce3adccd334037999ea56de27
kasparpoldemaa <kasparpoldemaa@gmail.com>
2022-04-29 10:28:17 +0300

    LoginView.vue

---
+++ src/main/java/com/example/demo/service/login/LoginService.java
@@ -0,0 +1,22 @@
+package com.example.demo.service.login;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class LoginService {
+
+//    @Resource
+//    private UserService userService;
+//
+//    @Resource
+//    private UserMapper userMapper;
+//
+//    @Resource
+//    private UserRoleService userRoleService;
+//
+//    @Resource
+//    private UserRoleMapper userRoleMapper;
+
+}
