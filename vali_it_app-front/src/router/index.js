7636158adefdcbd3ddf30b2a3a41d2de100bd6eb
annvool <annvool@gmail.com>
2022-05-03 16:18:44 +0300

    Added Event Page and works

--- vali_it_app-front/src/router/index.js
+++ vali_it_app-front/src/router/index.js
@@ -4,6 +4,7 @@ import HomeView from '../views/HomeView.vue'
 import LoginView from "@/views/LoginView";
 import UserPageView from "@/views/UserPageView";
 import SignUpView from "@/views/SignUpView";
+import EventView from "@/views/EventView";
 
 Vue.use(VueRouter)
 
@@ -27,6 +28,11 @@ const routes = [
     path: '/signup',
     name: 'sign-up',
     component: SignUpView
+  },
+  {
+    path: '/event',
+    name: 'event',
+    component: EventView
   },
 
 ]
---
+++ vali_it_app-front/src/views/EventView.vue
@@ -0,0 +1,71 @@
+<template>
+
+  <form>
+
+    <div class="form-group">
+      <label class="label-form">Kuupäev</label>
+      <input type="date" class="form-control" placeholder="Kuupäev" v-model="event.date">
+
+      <label class="label-form">Ürituse kellaaeg</label>
+      <input type="time" class="form-control" placeholder="Kellaaeg" v-model="event.time">
+
+      <label class="label-form">Ürituse pealkiri</label>
+      <input type="text" class="form-control" placeholder="Ürituse pealkiri" v-model="event.eventName">
+
+      <label class="label-form">Ettevõte</label>
+      <input type="text" class="form-control" placeholder="Ettevõtte nimi" v-model="event.company">
+
+      <label class="label-form">Ürituse koht</label>
+      <input type="text" class="form-control" placeholder="Kas klassis/zoom´i link." v-model="event.zoom">
+
+      <label class="label-form">Esineja</label>
+      <input type="text" class="form-control" placeholder="Esineja(te) nimi" v-model="event.presenterName">
+
+    </div>
+
+    <button class="btn btn-primary" v-on:click="addNewEvent">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "EventView",
+  data: function () {
+    return{
+
+      event:{}
+    }
+  },
+  methods:{
+    addNewEvent: function () {
+
+      this.$http.post("/event/add", this.event
+      ).then(response => {
+        console.log(response.data)
+      }).catch(error => {
+        console.log(error)
+      })
+    }
+  }
+
+
+}
+</script>
+
+<style scoped>
+form {
+  display: inline-block;
+  margin-right: 100px;
+}
+
+div.form-group {
+  display:grid;
+  grid-template-columns: max-content max-content;
+  grid-gap:10px;
+}
+div.form-group label       { text-align:right;
+  margin-top: 10px}
+div.form-group label:after { content: ":"; }
+
+
+</style>
