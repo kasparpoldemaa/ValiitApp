3506678631966e9efb3e987f85793308ba5eb220
J00nasKalda <joonask@outlook.com>
2022-05-09 13:51:59 +0300

    company page

--- src/main/java/com/example/demo/domain/internshipopportunity/InternshipOppurtunityService.java
+++ src/main/java/com/example/demo/domain/internshipopportunity/InternshipOppurtunityService.java
@@ -38,4 +38,6 @@ public class InternshipOppurtunityService {
 
         return companyResponse;
     }
+
+
 }
--- vali_it_app-front/src/views/CompanyPageView.vue
+++ vali_it_app-front/src/views/CompanyPageView.vue
@@ -1,55 +1,47 @@
 <template>
   <div>
 
-    <div class="btn-group">
-      <button type="button" style="background-color: blue" class="btn btn-dark" @click="getInternshipOfferDiv">Praktika
-        pakkumise lisamine
-      </button>
-      <button type="button" style="background-color: blue" class="btn btn-dark" @click="getAllOffersByUserId">Näita minu
-        loodud pakkumisi
-      </button>
-      <button type="button" style="background-color: blue" class="btn btn-dark" @click="getApplicationsDiv">Näita
-        kandideerimisi
-      </button>
+    <div>
+      <h2>SISESTA PRAKTIKA PAKKUMINE :</h2>
     </div>
-
-    <br>
-    <br>
     <form>
+      <div class="form-group">
+        <label class="label-form"></label>
+        <input type="text" class="form-control" id="name" placeholder="Asukoht" v-model="offer.location">
 
-      <div class="form-group" v-if="addOffer">
-
-        <label class="label-form">Asukoht</label>
-        <input type="text" class="form-control" placeholder="Asukoht" v-model="offer.location">
-
-        <label class="label-form">Algusaeg</label>
+        <label class="label-form"></label>
         <input type="text" class="form-control" placeholder="Millal praktika algab" v-model="offer.startTime">
 
-        <label class="label-form">Kestvus</label>
+        <label class="label-form"></label>
         <input type="text" class="form-control" placeholder="Praktika pikkus" v-model="offer.duration">
 
-        <label class="label-form">Vabad kohad</label>
+        <label class="label-form"></label>
         <input type="text" class="form-control" placeholder="Mitu praktika kohta on" v-model="offer.numberOfPositions">
 
-        <label class="label-form">Tehnoloogiad</label>
+        <label class="label-form"></label>
         <input type="text" class="form-control" placeholder="Kasutusel tehnoloogiad" v-model="offer.technology">
 
-        <label class="label-form">Töövorm</label>
+        <label class="label-form"></label>
         <input type="text" class="form-control" placeholder="Kontor, hübriid vm" v-model="offer.workType">
 
-        <label class="label-form">Lisainfo</label>
+        <label class="label-form"></label>
         <input type="text" class="form-control" placeholder="Täiendav info" v-model="offer.comment">
 
       </div>
 
-      <div class="form-group" v-if="applications">
+      <div>
 
-        <label class="label-form">Asukoht</label>
-        <input type="text" class="form-control" placeholder="Asukoht" v-model="offer.location">
+        Praktikakoht on tasustatud € <input type="checkbox" v-model="isPayable">
+        <br>
+        <br>
+        <button class="btn btn-primary" v-on:click="addInternship">Lisa uus pakkumine</button>
 
       </div>
 
-      <div class="xxx" v-if="showOffers">
+      <br>
+      <br>
+
+      <div class="xxx">
         <div>
           <div id="accordion">
             <div class="card">
@@ -86,21 +78,33 @@
                       </tr>
                       </thead>
                       <tbody>
-                      <tr v-for="offerForm in offerForms" >
+                      <tr v-for="offerForm in offerForms">
                         <th scope="row">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left-fill" viewBox="0 0 16 16">
-                          <path d="M2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/></svg>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                               class="bi bi-chat-left-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                          </svg>
                         </th>
                         <td>{{ offerForm.startTime }}</td>
                         <td>{{ offerForm.duration }}</td>
                         <td>{{ offerForm.location }}</td>
                         <td>{{ offerForm.numberOfPositions }}</td>
                         <td>{{ offerForm.technology }}</td>
-                        <td>{{offerForm.workType }}</td>
-                        <td>{{ offerForm.isPayable }}</td>
+                        <td>{{ offerForm.workType }}</td>
+                        <td id="wrapthisthing" style="word-wrap: break-word;">{{ offerForm.isPayable ? 'jah' : 'ei' }}</td>
                         <td>{{ offerForm.comment }}</td>
                         <td>
-                          <button type="button" class="btn btn-danger" @click="deleteInternshipOfferByID(offerForm.id)">Kustuta</button>
+                          <button type="button" class="btn btn-danger" @click="deleteInternshipOfferByID(offerForm.id)">
+                            Kustuta
+                          </button>
+                        </td>
+                        <td>
+                          <button type="button" class="btn btn-primary">
+                            Huvilised <span class="badge badge-light">{{offerForm.id}}</span>
+                            <span class="sr-only">how many applicants</span>
+                          </button>
+
                         </td>
                       </tr>
                       </tbody>
@@ -109,20 +113,10 @@
                   </div>
                 </div>
 
-
               </div>
             </div>
           </div>
         </div>
-
-      </div>
-
-      <div v-if="addOffer">
-
-        Praktikakoht on tasustatud € <input type="checkbox" v-model="isPayable">
-        <br>
-        <br>
-        <button class="btn btn-primary"  v-on:click="addInternship">Lisa uus pakkumine</button>
 
       </div>
 
@@ -151,36 +145,24 @@ export default {
       answer: ''
 
 
-
     }
   },
   methods: {
 
-    // answerToBoolean: function () {
-    //   if(this.isPayable===true){
-    //     this.answer = 'jah'
-    //   }else{
-    //     this.answer = 'ei'
-    //   }
+    // getApplicationsDiv: function () {
+    //   this.applications = true
+    //   this.addOffer = false
+    //   this.showOffers = false
     // },
 
+    // getShowOffersDiv: function () {
+    //   this.showOffers = true
+    //   this.applications = false
+    //   this.addOffer = false
+    // },
 
-    getInternshipOfferDiv: function () {
-      this.addOffer = true
-      this.applications = false
-      this.showOffers = false
-    },
-
-    getApplicationsDiv: function () {
-      this.applications = true
-      this.addOffer = false
-      this.showOffers = false
-    },
-
-    getShowOffersDiv: function () {
-      this.showOffers = true
-      this.applications = false
-      this.addOffer = false
+    refreshPage: function () {
+      window.location.reload();
     },
 
     addInternship: function () {
@@ -191,7 +173,8 @@ export default {
             }
           }
       ).then(response => {
-        // this.clearFields()
+        this.refreshPage()
+        this.getAllOffersByUserId()
         console.log(response.data)
       }).catch(error => {
         console.log(error)
@@ -205,7 +188,6 @@ export default {
             }
           }
       ).then(response => {
-        this.getShowOffersDiv()
         this.arrayLength = response.data.length
         this.offerForms = response.data
         console.log(response.data)
@@ -226,6 +208,11 @@ export default {
         console.log(error)
       })
     },
+  },
+  mounted() {
+    this.addInternship()
+    this.getAllOffersByUserId()
+
   }
 }
 </script>
@@ -241,36 +228,38 @@ export default {
 
 form {
   display: inline-block;
-  margin-right: 100px;
+
 }
 
 div.form-group {
-  display: grid;
-  grid-template-columns: max-content max-content;
-  grid-gap: 10px;
+  display: inline-list-item;
+  margin: 10px;
+
 }
 
-div.form-group label {
-  text-align: right;
-  margin-top: 10px
-}
+/*div.form-group label {*/
+/*  text-align: right;*/
+/*  margin-top: 1px*/
+/*}*/
 
-div.form-group label:after {
-  content: ":";
-}
+/*div.form-group label:after {*/
+/*  content: ":";*/
+/*}*/
 
 .card {
   width: 80vw;
   background-color: azure;
   color: black;
   border-color: black;
-  left: 4%;
 }
 
 .card-header {
   color: black;
 }
 
+#wrapthisthing{
+  max-width: 300vw;
+}
 
 
 </style>
