db2d4d82baa0e21f5b915c6ab6a5396db3ca0a18
annvool <annvool@gmail.com>
2022-05-09 09:28:57 +0300

    Added @AllArgs ja @NoArgs

--- src/main/java/com/example/demo/domain/workexperience/WorkExperienceDto.java
+++ src/main/java/com/example/demo/domain/workexperience/WorkExperienceDto.java
@@ -11,6 +11,7 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class WorkExperienceDto implements Serializable {
+
     private Integer id;
     private LocalDate startDate;
     private LocalDate endDate;
