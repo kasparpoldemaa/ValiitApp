f4e02234cfd5f6780c95d553c49f8409dc14d494
annvool <annvool@gmail.com>
2022-05-09 09:27:37 +0300

    Added @AllArgs ja @NoArgs

--- src/main/java/com/example/demo/domain/educationexperience/EducationExperienceDto.java
+++ src/main/java/com/example/demo/domain/educationexperience/EducationExperienceDto.java
@@ -1,13 +1,17 @@
 package com.example.demo.domain.educationexperience;
 
 import com.example.demo.domain.student.StudentDto;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.lang.Nullable;
 
 import java.io.Serializable;
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class EducationExperienceDto implements Serializable {
 
     private Integer id;
