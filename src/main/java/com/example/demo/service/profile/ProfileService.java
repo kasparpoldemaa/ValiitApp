567297607e635deec2d136890e652c347e846062
J00nasKalda <joonask@outlook.com>
2022-05-11 16:28:15 +0300

    company - get applicants NOT WORKING

--- src/main/java/com/example/demo/service/profile/ProfileService.java
+++ src/main/java/com/example/demo/service/profile/ProfileService.java
@@ -1,12 +1,16 @@
 package com.example.demo.service.profile;
 
+import com.example.demo.domain.contact.Contact;
 import com.example.demo.domain.contact.ContactService;
+import com.example.demo.domain.internshipapplicant.InternshipApplicant;
+import com.example.demo.domain.internshipapplicant.InternshipApplicantService;
 import com.example.demo.domain.student.StudentService;
 import com.example.demo.domain.studentprofile.StudentProfileDto;
 import com.example.demo.domain.studentprofile.StudentProfileService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,17 +25,32 @@ public class ProfileService {
     @Resource
     private StudentService studentService;
 
+    @Resource
+    private InternshipApplicantService internshipApplicantService;
+
     public void addNewUserProfile(StudentProfileDto profileDto) {
         // lisa andmed
             //  student profile
             //  contact
     }
 
-    public List<StudentName> getListOfStudentProfiles(List<Integer> studentIds) {
-        return studentService.getStudents(studentIds);
-    }
+
 
     public List<ApplicantResponse> getAllApplicants(Integer opportunityId) {
-        return null;
+        List<InternshipApplicant> applicants = internshipApplicantService.getApplicants(opportunityId);
+        List<ApplicantResponse> responses = new ArrayList<>();
+        for (InternshipApplicant applicant : applicants) {
+            Contact contact = contactService.getContact(applicant.getStudent().getUser().getId());
+            ApplicantResponse applicantResponse = new ApplicantResponse();
+            applicantResponse.setStudentId(applicant.getStudent().getId());
+            applicantResponse.setProfileId(applicant.getStudent().getStudentProfile().getId());
+            applicantResponse.setFirstName(contact.getFirstName());
+            applicantResponse.setLastName(contact.getLastName());
+            responses.add(applicantResponse);
+        }
+        return responses;
+
+
+
     }
 }
